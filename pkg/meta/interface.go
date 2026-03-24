0848aa430f34cf842b0dadb0749823e27a9db05b
jiefenghuang <jiefeng@juicedata.io>
2026-03-24 17:57:35 +0800

    dep: update go version to 1.24 (#6680)
    Signed-off-by: jiefenghuang <jiefeng@juicedata.io>

--- pkg/meta/interface.go
+++ pkg/meta/interface.go
@@ -645,7 +645,7 @@ func NewClient(uri string, conf *Config) Meta {
 	driver := uri[:p]
 	if driver == "mysql" || driver == "postgres" {
 		if uri, err = setPasswordFromEnv(uri); err != nil {
-			logger.Fatalf(err.Error())
+			logger.Fatal(err.Error())
 		}
 	}
 	logger.Infof("Meta address: %s", utils.RemovePassword(uri))
